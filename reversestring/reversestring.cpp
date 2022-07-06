9735d94ced29ea6c77ef554bc83aba632fb1eec1
Victor Muljo <muljo@usc.edu>
2022-07-06 15:30:40 -0700

    ReversedString code

---
+++ reversestring/reversestring.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <iterator>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+// Program to reverse a string using iterators and queue data structure.
+// Sets to look for another way to reverse a string instead of the iterating from last string element
+
+//Function that reverses a string the original way (O(n) time complexity)
+string reverseStringOG(string str){
+    string reversed;
+    // cout << str.size();
+    for(int i = str.size()-1; i >= 0; i--){
+        reversed.push_back(str.at(i));
+    }
+    return reversed;
+}
+
+//Function that reverses a string using a stack (O(n) time complexity)
+string reverseStringStack(string word){
+    stack<char> str;
+    // Loops through the string to push each char into the stack
+    for(int i = 0; i < word.size(); i++){
+        str.push(word.at(i)); 
+    }
+    word.clear(); //Clears the string so it can be loaded in
+
+    // While the stack is not empty
+    while (!str.empty()){
+        char letter = str.top(); // Gets the char at the end of the stack, which should be the letter at the end of string
+        word.push_back(letter); // Use push_back() to add a single letter to the string. Time complexity O(1)
+        str.pop(); //Removes the letter at the top
+    }
+
+    return word;
+}
+
+int main(){
+    string word;
+    cout << "Enter a word: ";
+    cin >> word;
+    cout << "Initial word: " << word << endl;
+
+    //Prints out the reversed word using normal method
+    cout << "Reversed Word OG: " << reverseStringOG(word) << endl;
+
+    //Prints out the reversed word using stack data structure
+    cout << "Reversed Word: " << reverseStringStack(word) << endl;
+    
+}
