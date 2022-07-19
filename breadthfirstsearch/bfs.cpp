06be3dd99a4bb05f0f9eac6c7d0096838ee8e472
Victor Muljo <muljo@usc.edu>
2022-07-18 17:53:19 -0700

    BFS cleanup + more comments

--- breadthfirstsearch/bfs.cpp
+++ breadthfirstsearch/bfs.cpp
@@ -1,12 +1,11 @@
 // Program to practice using the breadth-first search algorithm
-// Use of Queue data structure and Graph using adjacency list (list of linked list)
+// Use of Queue data structure and Graph using unordered map (hash-table) for O(1) lookup.
 
 #include "graph.h"
-// #include <utility>
 #include <math.h>
 #include <queue>
-#include <set>
 
+// BFS Algorithm function
 template<typename V>
 void BFSAlgo(Graph<V> g, V &start){
     unordered_map<V, vector<V>> graph = g.graph;
@@ -17,26 +16,32 @@ void BFSAlgo(Graph<V> g, V &start){
     for(typename unordered_map<V, vector<V>>::iterator it = graph.begin(); it != graph.end(); ++it){
         search[it->first] = make_pair(false,-1);
     }
-    queue<V>* bfsq = new queue<V>;
-    bfsq->push(start);
-    search[start] = make_pair(true, 0); //sets first vertex to true to indicate visited vertex
+    
+    queue<V>* bfsq = new queue<V>; // Initializes queue for BFS
+    bfsq->push(start); // Adds the start vertex set by user as first vertex to search
+    search[start] = make_pair(true, 0); //sets first vertex to true to indicate visited vertex with distance of 0
     pred[start] = NULL;
-    V currVertex;
+    V currVertex; // Initializes current vertex in the search
+
+    // While the BFS queue is not empty (should never be empty until the last vertex is completed)
     while(!bfsq->empty()){
-        currVertex = bfsq->front();
+        // Sets the first value in the BFS queue as the current vertex to search and remove from queue
+        currVertex = bfsq->front(); 
         bfsq->pop();
+
+        // Iterates through each adjacent vertex in the vertex's list of edges
         for(typename vector<V>::iterator it = graph[currVertex].begin(); it != graph[currVertex].end(); ++it){
-            if(search[*it].first == false){ //want to check if in list or not
+            if(search[*it].first == false){ //want to check if the vertex has not been marked (marked/true indicates a visited vertex already)
                 bfsq->push(*it); // Adds each adj vertex to queue
-                search[*it].first = true; //Sets as true to indicate the node has been visited
-                search[*it].second = search[currVertex].second + 1;
-                pred[*it] = currVertex;
+                search[*it] = make_pair(true, search[currVertex].second + 1); //Sets as true to indicate the node has been visited and increase depth by 1 to indicate distance from start vertex
+                pred[*it] = currVertex; // Stores predecessor vertex
             } 
         }
     }
 
-    delete bfsq;
+    delete bfsq; // Deallocates bfsq queue from heap
 
+    // Iterates through each vertex to print its predecessor and distance from start vertex
     for(typename unordered_map<V, pair<bool, int>>::iterator it = search.begin(); it != search.end(); ++it){
         cout << "Vertex: " << it->first << endl;
         cout << "Predecessor: " << pred[it->first] << endl;
@@ -49,8 +54,9 @@ int main(){
     Graph<int> g = exampleGraph();
     // vector<int> t = g.getAdjVertex(0);
     unordered_map<int, vector<int>> test = g.graph;
-    // g.removeVertex(2);
-    unordered_map<int, vector<int>>::iterator it = g.graph.begin();
-    int start = it->first;
+    // g.printGraph();
+    // unordered_map<int, vector<int>>::iterator it = g.graph.begin();
+    unordered_map<int, vector<int>>::iterator itt = g.graph.find(0);
+    int start = itt->first;
     BFSAlgo(g, start);
 }
